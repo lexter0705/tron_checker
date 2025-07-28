e90c5b048062f56fb085f992f31afbf897864db5
Alexey <88457801+lexter0705@users.noreply.github.com>
2025-07-28 12:34:00 +0300

    Update connector.py

--- tron/connector.py
+++ tron/connector.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from tronpy.async_tron import AsyncTron, AsyncHTTPProvider
 
 from tron.wallet_info import WalletInfo
@@ -10,7 +8,7 @@ class TronConnector:
         provider = AsyncHTTPProvider(endpoint_uri, *args, **kwargs)
         self.__tron = AsyncTron(provider)
 
-    async def get_wallet(self, address: str) -> Optional[WalletInfo]:
+    async def get_wallet(self, address: str) -> WalletInfo:
         balance = await self.__tron.get_account_balance(address) / 1_000_000
         bandwidth = await self.__tron.get_bandwidth(address)
         energy = await self.__tron.get_energy(address)
